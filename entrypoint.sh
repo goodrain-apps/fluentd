677a249c0830a6ea24568e48bddc8871d6907cd9
week2311 <89837333+week2311@users.noreply.github.com>
2022-05-16 13:35:25 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
 
-export SERVICE_NAME=nginx
-export ES_ADDR=http://9200.gr8ed95a.duaqtz0k.17f4cc.grapps.cn/
-export ES_USER=elastic
-export ES_PASS=MagicWord
-export SERVICE_LOGS=/var/log/nginx/access.log,/var/log/nginx/error.log
-
 auto_envsubst_test(){
     defined_envs=$(printf '${%s} ' $(env | cut -d= -f1))
     template=/fluent.conf
@@ -15,5 +9,5 @@ auto_envsubst_test(){
 }
 auto_envsubst_test
 
-echo "$(sed 's/[0-9].*/114.114.114.114/g' /etc/resolv.conf)" > /etc/resolv.conf
+
 fluentd -c /fluentd/etc/fluent.conf
